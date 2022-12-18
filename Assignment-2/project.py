a133d38751b4b44859f93deb32aafe647d31fd69
Mahadi445 <119592889+Mahadi445@users.noreply.github.com>
2022-12-18 19:01:04 +0600

    Update project.py

--- Assignment-2/project.py
+++ Assignment-2/project.py
@@ -1,39 +1,51 @@
-#=============Create BMI Calculator Project In Python===================#
+#Simple the_BMI calculator using python::
 
-#===========calculate Inche and Feet convert to centimeters===========#
-Inch_per_feet = 12
-cm_per_inch = 2.54
-feet = int(input("Enter Your Height Number of feets:"))
-inch = int(input("Enter Your Height Number Of Inches:"))
-Height_cm = (feet*Inch_per_feet+inch)*cm_per_inch
-print("Here Your Centimeters Number:",Height_cm)
-print("============================================")
+# define a function name convert to feet,inche  ::
+def convert():
+    print("Input your Informations-")
+    h_ft = int(input("Feet: "))
+    h_inch = int(input("Inches: "))
+    
+    h_inch += h_ft * 12
+    h_cm = round(h_inch * 2.54, 1)
+    print("Your height is : %d cm." % h_cm)
 
-
-#============user Input Height and weight===============#
-Height =float(input("Enter Your Height in Centimeters:"))
-Weight =int(input("Enter Your Weight in KG:"))
+convert()
 
 
-#==========Set Formula Height And weight=======#
-Height = Height/100
-BMI = Weight/float((Height*Height))
-print("Here Your Body Mass Index :",BMI)
+# define a function name The_BMI::
+
+def the_BMI(height, weight):
+    return round((weight /(height/100)**2),2)
+
+# asking for input from the users::
+
+Height = float(input("Enter your height in cm: "))
+Weight = float(input("Enter your weight in kg: "))
+
+# print to welcome calculator::
+
+print("Welcome to the the_BMI calculator.")
 
 
-#=============conditon apply For BMI===========#
-if (BMI<18.5):
-    print('Your Health is Underweight')
-elif (BMI>=18.5) and (BMI<25):
-    print("Your Health is Normal")
-elif (BMI>=25) and (BMI<30):
-    print('Your Health is Overweight')
-elif (BMI>=30):
-    print('Your Health is Obesity')
+# print Body Mass Index::
+
+the_BMI = the_BMI(Height, Weight)
+print("Your the_BMI is: ", the_BMI)
+
+
+# using the if-elif-else conditions::
+
+if (the_BMI<18.5):
+    print('Oops! You are underweight.')
+elif (the_BMI>=18.5) and (the_BMI<25):
+    print("Awesome! You are healthy.")
+elif (the_BMI>=25) and (the_BMI<30):
+    print('Eee! You are overweight')
+elif (the_BMI>=30):
+    print('Seesh! You are obese.')
 else:
-    print("Enter Your Vaild Number")    
-                 
-
+    print("Enter Your Vaild Number") 
 
 
 
